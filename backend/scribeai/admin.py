f0da521361c0425ed856514708a7451dad2dd057
Dylan <dtro18@gmail.com>
2024-10-25 10:30:29 -0700

    Revert "Initial commit"
    This reverts commit f708b8b82a9f308e175f7a8654acd81ef5c82109.

--- backend/scribeai/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Patient
-
-class PatientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'id')
-
-# Register your models here.
-
-admin.site.register(Patient, PatientAdmin)
